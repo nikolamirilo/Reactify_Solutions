40f3abf4aeee6755412183f7d69000eac5525c26
Nikola Mirilo <nikolamirilo@gmail.com>
2024-06-26 20:25:46 +0200

    added brands

--- components/Brands/index.tsx
+++ components/Brands/index.tsx
@@ -2,46 +2,53 @@ import { Brand } from "@/types/brand";
 import Image from "next/image";
 
 const brandsData: Brand[] = [
-  {
-    id: 1,
-    name: "UIdeck",
-    href: "https://uideck.com",
-    image: "/images/brands/uideck.svg",
+
+  {  
+id: 1,
+name: "",
+href: "",  
+     image: "/images/brands/Arioso_Investment.png" 
   },
-  {
-    id: 2,
-    name: "Tailgrids",
-    href: "https://tailgrids.com",
-    image: "/images/brands/tailgrids.svg",
-  },
-  {
-    id: 3,
-    name: "Lineicons",
-    href: "https://lineicons.com",
-    image: "/images/brands/lineicons.svg",
-  },
-  {
-    id: 4,
-    name: "GrayGrids",
-    href: "https://graygrids.com",
-    image: "/images/brands/graygrids.svg",
-  },
-  {
-    id: 5,
-    name: "TailAdmin",
-    href: "https://tailadmin.com",
-    image: "/images/brands/tailadmin.svg",
-  },
-];
+    {
+ id: 1,
+ name: "",
+ href: "",   
+      image: "/images/brands/C2S.png" 
+    },
+      {
+ id: 1,
+ name: "",
+ href: "",   
+      image: "/images/brands/Claire.png" 
+    },
+      {
+ id: 1,
+ name: "",
+ href: "",   
+      image: "/images/brands/Montre.png" 
+    },
+      {
+ id: 1,
+ name: "",
+ href: "",   
+      image: "/images/brands/Swapabee.jpg" 
+    },
+      {
+ id: 1,
+ name: "",
+ href: "",   
+      image: "/images/brands/WeBuyInDubai.png"
+}];
+
 
 const Brands = () => {
   return (
     <section className="pt-16">
       <div className="container">
         <div className="-mx-4 flex flex-wrap">
-          <div className="w-full px-4">
+          <div className="w-full px-4 mx-auto">
             <div
-              className="wow fadeInUp flex flex-wrap items-center justify-center rounded-md bg-dark py-8 px-8 dark:bg-primary dark:bg-opacity-5 sm:px-10 md:py-[40px] md:px-[50px] xl:p-[50px] 2xl:py-[60px] 2xl:px-[70px]"
+              className="wow fadeInUp flex items-center justify-center rounded-md bg-dark py-8 px-8 dark:bg-primary dark:bg-opacity-5 sm:px-10 md:py-[40px] md:px-[50px] xl:p-[50px] 2xl:py-[60px] 2xl:px-[70px]"
               data-wow-delay=".1s
               "
             >
@@ -67,9 +74,9 @@ const SingleBrand = ({ brand }: { brand: Brand }) => {
         href={href}
         target="_blank"
         rel="nofollow noreferrer"
-        className="relative h-10 w-full opacity-70 grayscale transition hover:opacity-100 hover:grayscale-0 dark:opacity-60 dark:hover:opacity-100"
+        className="relative opacity-70 grayscale transition hover:opacity-100 hover:grayscale-0 dark:opacity-60 dark:hover:opacity-100"
       >
-        <Image src={image} alt={name} fill />
+        <Image src={image} alt={name} width={200} height={200} className="rounded-full" />
       </a>
     </div>
   );
