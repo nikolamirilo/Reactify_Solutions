f5323970bbd9495a1e87f57058dbbd82d81613a6
Nikola <59510666+nikolamirilo@users.noreply.github.com>
2024-12-30 13:49:26 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default async function page () {
       <AboutSectionOne />
       <Services />
       <Technologies />
-      <Solutions />
+      {/* <Solutions /> */}
       {/* <Video /> */}
       <Clients />
       <Testimonials />
